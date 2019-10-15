3b668342ba4f58b7f1367bb9d53a8f5138df8394
CarbonMass <sina202@gmail.com>
2019-10-15 15:59:00 -0700

    Aloha - Final Version.

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 
-document.addEventListener("DOMContentLoaded", function() {    const elem = document.querySelector('.main-carousel');
-const flkty = new Flickity( elem, {
-  // options
-  cellAlign: 'left',
-  wrapAround: true,
-  freeScroll: true,
+document.addEventListener("DOMContentLoaded", function () {
+  const elem = document.querySelector('.main-carousel');
+  const flkty = new Flickity(elem, {
+    // options
+    cellAlign: 'left',
+    wrapAround: true,
+    freeScroll: true,
+    autoPlay: true,
+    autoPlay: 2000
+  })
 
-  autoPlay: true,
-  autoPlay: 2000
+  const emailInput = document.getElementById("subscribe-form-input");
 
-})});
+  document.querySelector(".subscribe-form").addEventListener("submit", function (event) {
+    event.preventDefault();
+    
+    if (emailInput.value === "") {
+      alert("Please enter a valid email address");
+    } else {
+      alert("Thanks for subscribing!");
+    }
+  });
+}
+);
